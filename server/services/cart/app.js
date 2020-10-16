cd486482d23f4426a854fce464e394cdb80acd12
parasaraib <manikparasara@ymail.com>
2020-10-16 12:24:03 +0800

    skeleton code

---
+++ server/services/cart/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 3002;
+const app = express();
+
+// Body Parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+
+// app.listen(port, () => {
+//   console.log(`User Server listening at http://localhost:${port}`);
+// });
+
+module.exports = app;
