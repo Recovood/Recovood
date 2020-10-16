f8bf5d67b3342534b10792c87eafbd10a85b923d
Brando Juberd <brandowalandouw@gmail.com>
2020-10-16 19:58:32 +0700

    cart completed need testing

--- server/services/cart/app.js
+++ server/services/cart/app.js
@@ -3,13 +3,17 @@ const cors = require("cors");
 
 const port = process.env.PORT || 3002;
 const app = express();
+const router = require("./routes")
+
+const errorHandler = require("./middlewares/errorHandler")
+app.use(cors());
 
 // Body Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/', router)
 
-app.use(cors());
-
+app.use(errorHandler)
 // app.listen(port, () => {
 //   console.log(`User Server listening at http://localhost:${port}`);
 // });
---
+++ server/services/cart/routes/index.js
@@ -0,0 +1,16 @@
+const CartController = require('../controllers/cartController')
+
+const router = require('express').Router()
+
+router.get("/", (req, res) => {
+  res.send("Cart API")
+})
+
+router.post("/carts", CartController.create)
+router.get("/carts", CartController.getCarts)
+router.patch("/carts/:id", CartController.updateQuantity)
+router.delete("/carts/:id", CartController.deleteCart)
+router.patch("/checkout", CartController.checkout)
+
+
+module.exports = router
