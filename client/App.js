6656e6c89e7e06e1378ceea53807d8abac24c8b7
merchikarzk <merchika11@gmail.com>
2020-10-19 08:49:22 +0700

    half finished

--- client/App.js
+++ client/App.js
@@ -1,28 +1,28 @@
-import PopupDetail from "./components/PopupDetail"
-import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet, Text, View } from "react-native";
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 // import { Provider } from "react-redux";
 // import store from "./store/index";
 import { Cart, Login, FoodList, FoodDetails } from "./screens/index";
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    // <Provider>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Cart" component={Cart} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="FoodList" component={FoodList} />
-        <Stack.Screen name="FoodDetails" component={FoodDetails} />
-        <PopupDetail/>
-      </Stack.Navigator>
-    </NavigationContainer>
-    // </Provider>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login"  screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="FoodList" component={FoodList} />
+          <Stack.Screen name="FoodDetails" component={FoodDetails} />
+          <Stack.Screen name="Cart" component={Cart} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
---
+++ client/components/Button.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, Text, StyleSheet} from 'react-native'
+
+
+const Button = ({content}) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{content}</Text>
+        </View>
+    )
+}
+
+export default Button
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: 51,
+        borderRadius: 100,
+        backgroundColor: '#376443',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    text: {
+        color: "white"
+    }
+})
