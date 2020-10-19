b4ac57fdd4c94b99ccc27d2750e3128652687010
ArdySitio <ardy.ai29@gmail.com>
2020-10-19 18:09:48 +0700

    Update server

--- server/services/cart/tests/cart.test.js
+++ server/services/cart/tests/cart.test.js
@@ -1,35 +1,69 @@
 const request = require("supertest");
 const app = require("../app.js");
+const { sequelize, User } = require("../models");
+const { queryInterface } = sequelize;
 
 let user;
+let idOfCart;
 
 beforeAll(async (done) => {
+  const carts = {
+    FoodId: 1,
+    quantity: 20,
+    status: "waiting for payment",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
   user = await User.create({
     username: "user",
     email: "user@mail.com",
     password: "userganteng",
     role: "customer",
   });
+
+  queryInterface
+    .bulkInsert("Carts", [carts], {})
+    .then(() => {
+      request(app)
+        .post("/carts")
+        .send({
+          FoodId: 1,
+          quantity: 20,
+          status: "waiting for payment",
+        })
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .then((res) => {
+          return (idOfCart = res.body.id);
+        });
+    })
+    .then(() => {
+      done();
+    });
 });
 
+afterAll((done) => {
+  queryInterface.bulkDelete("Carts", null, {}).then(() => done());
+  queryInterface.bulkDelete("Users", null, {}).then(() => done());
+});
+
+console.log(user, ">>> user di cart");
+
 describe("MODEL Cart", () => {
-  // test("Hello Cart", (done) => {
-  //   request(app)
-  //     .get("/")
-  //     .set("Accept", "application/json")
-  //     // .expect("Content-Type", /json/)
-  //     .then(response => {
-  //       const { body, status } = response
-  //       expect(body).toBe("Cart API")
-  //     })
-  // })
-  test("Create Cart", (done) => {
+  test("Success Create Cart", (done) => {
     request(app)
-      .post("/")
+      .post("/carts")
+      .send({})
       .set("Accept", "application/json")
+      .set("user_id", user.id)
       .expect("Content-Type", /json/)
       .then((response) => {
         const { body, status } = response;
+        idOfCart = body.id;
+        expect(status).toBe(201);
+        expect(response).toHaveProperty("body", expect.any(Object));
+        done();
       });
   });
 });
